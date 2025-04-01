68ace430d23c41b465e462da6c2a499d33ffd966
Paul Nguyen <paul4pixar@gmail.com>
2025-04-01 23:30:24 +0000

    added outlet to account and Bookmarks

--- final-project-1/src/components/Account.js
+++ final-project-1/src/components/Account.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 
 const Account = () => {
@@ -52,6 +52,7 @@ const Account = () => {
                     </div>
                 </div>
             </div>
+            <Outlet />
         </div>
     )
 };
--- final-project-1/src/components/Bookmarks.js
+++ final-project-1/src/components/Bookmarks.js
@@ -3,9 +3,15 @@ import React from 'react';
 const Bookmarks = () => {
     return (
         <div>
-            
+            <h1>Bookmarks Page</h1>
+            <p>This is a test paragraph to check if the component is rendering.</p>
+            <div>
+                <div>Bookmark 1</div>
+                <div>Bookmark 2</div>
+                <div>Bookmark 3</div>
+            </div>
         </div>
-    )
+    );
 };
 
 export default Bookmarks;
