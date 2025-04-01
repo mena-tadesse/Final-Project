abb69bb188af1a278e0ca383730c8d25604cb5c6
Paul Nguyen <paul4pixar@gmail.com>
2025-04-01 20:58:44 +0000

    Added Calendar

--- final-project-1/src/components/Calendar.js
+++ final-project-1/src/components/Calendar.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react'; 
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 const Calendar = () => {
     return (
         <div>
-            
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                events={[
+                    { title: 'Event 1', date: '2025-04-01' },
+                    { title: 'Event 2', date: '2025-04-15' },
+                ]} 
+            />
         </div>
-    )
+    );
 };
 
 export default Calendar;
