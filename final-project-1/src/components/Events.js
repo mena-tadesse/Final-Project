c00e38e692ab15bbd0d6eb100ee92bc5f2d8b689
Sydney <alee164@charlotte.edu>
2025-04-15 23:29:13 +0000

    css changes

--- final-project-1/src/components/Events.js
+++ final-project-1/src/components/Events.js
@@ -216,10 +216,12 @@ const Events = () => {
                         ★
                     </span>
                     <img src={event.images?.[0]?.url} alt={event.name} />
+                    <div className='event-content'> 
                         <h3>{event.name}</h3>
-                        <p>{event.dates?.start?.localDate}</p>
-                        <p>{event.classifications?.[0]?.segment?.name}</p>
+                        <p>Time: {event.dates?.start?.localDate}</p>
+                        <p>Category: {event.classifications?.[0]?.segment?.name}</p>
                         <Link to={`/events/${event.id}`}>View Details</Link>
+                        </div>
                     </div>
                     ))
                 ) : (
